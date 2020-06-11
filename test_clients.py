67ef8ed38d71c153efb925e882c6d4476fe90418
Donaventuro <donaventuro@gmail.com>
2020-06-11 13:26:45 +0500

    'release'

--- test_clients.py
+++ test_clients.py
@@ -10,13 +10,6 @@ clients = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(MAX_
 for client in clients:
 
     threading.Thread(target=client.connect, args=(address_to_server,)).start()
-    # client.connect(address_to_server)
-    #
-    # threading.Thread(target=).start
-    #
-
-for i in range(MAX_CONNECTIONS):
-    clients[i].send(bytes("hello from client number " + str(i), encoding='UTF-8'))
 
 for client in clients:
     data = client.recv(1024)
--- test_methods.py
+++ test_methods.py
@@ -2,13 +2,13 @@ import requests
 
 
 def test():
-    print("Проверка метода get " + str(requests.get('https://httpbin.org/')))
-    print("Проверка метода post " + str(requests.post('https://httpbin.org/post', data={'key': 'value'})))
-    print("Проверка метода put " + str(requests.put('https://httpbin.org/put', data={'key': 'value'})))
-    print("Проверка метода delete " + str(requests.delete('https://httpbin.org/delete')))
-    print("Проверка метода head " + str(requests.head('https://httpbin.org/get')))
-    print("Проверка метода patch " + str(requests.patch('https://httpbin.org/patch', data={'key': 'value'})))
-    print("Проверка метода options " + str(requests.options('https://httpbin.org/get')))
+    print("Проверка метода get " + str(requests.get('http://localhost:9000/')))
+    print("Проверка метода post " + str(requests.post('http://localhost:9000/post', data={'key': 'value'})))
+    print("Проверка метода put " + str(requests.put('http://localhost:9000/put', data={'key': 'value'})))
+    print("Проверка метода delete " + str(requests.delete('http://localhost:9000/delete')))
+    print("Проверка метода head " + str(requests.head('http://localhost:9000/get')))
+    print("Проверка метода patch " + str(requests.patch('http://localhost:9000/patch', data={'key': 'value'})))
+    print("Проверка метода options " + str(requests.options('http://localhost:9000/get')))
 
 
 if __name__ == '__main__':
